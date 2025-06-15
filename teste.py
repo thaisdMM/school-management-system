a9402832dbd386958188dcf8f2b8422de093c05a
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-06-15 18:29:31 +0200

    testing path functions and collection data function

--- teste.py
+++ teste.py
@@ -1,14 +1,14 @@
 from library.files import path
 from library.files import project_file
+from library.students import students_functions
 
 
-caminho2 = "files_created/cadastro_alunos_matricula.json"
+# caminho2 = "files_created/cadastro_alunos_matricula.json"
 
-arquivo = path.absolute_path(caminho2)
-print(arquivo)
-conteudo = project_file.ler_arquivo(arquivo)
-print(conteudo)
-
+# arquivo = path.absolute_path(caminho2)
+# print(arquivo)
+# conteudo = project_file.ler_arquivo(arquivo)
+# print(conteudo)
 
 # caminho2 = "files_created/cadastro_alunos_matricula.json"
 # arquivo = path.absolute_path(caminho2)
@@ -17,3 +17,13 @@ print(conteudo)
 # print(new_file)
 # conteudo = project_file.ler_arquivo(new_file)
 # print(conteudo)
+
+arquivo_estudantes = students_functions.students_file_name()
+print(arquivo_estudantes)
+caminho_absoluto_arquivo = path.absolute_file_path(arquivo_estudantes)
+print(caminho_absoluto_arquivo)
+print(project_file.verificar_arquivo_existe(caminho_absoluto_arquivo))
+print(project_file.verificar_pasta_existe())
+
+dados_alunos = students_functions.coleta_dados_alunos()
+print(dados_alunos)
