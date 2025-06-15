97a530683a0e2fafc7860d97a5e209d7fe195ca1
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-06-15 15:12:20 +0200

    Update teste.py

--- teste.py
+++ teste.py
@@ -8,3 +8,12 @@ arquivo = path.absolute_path(caminho2)
 print(arquivo)
 conteudo = project_file.ler_arquivo(arquivo)
 print(conteudo)
+
+
+# caminho2 = "files_created/cadastro_alunos_matricula.json"
+# arquivo = path.absolute_path(caminho2)
+# new_file = project_file.criar_subscrever_arquivo(arquivo)
+
+# print(new_file)
+# conteudo = project_file.ler_arquivo(new_file)
+# print(conteudo)
