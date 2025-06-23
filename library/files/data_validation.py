2c5cba8060cd9fb385945d834ed3080439095c27
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-06-23 14:56:45 +0200

    Create data_validation.py

---
+++ library/files/data_validation.py
@@ -0,0 +1,65 @@
+from library.files import project_file
+from library.files import path
+from library.students import students
+from library.grades import grades
+
+
+# Função mais genérica que a função abaixo, ver se ela terá utilidade no futuro ou apagar
+def verifica_conteudo_arquivo(file_path):
+    conteudo = project_file.ler_arquivo(file_path)
+    if conteudo is None:
+        print(
+            "Não foi possível carregar os dados dos alunos e/ou disciplinas. O arquivo não existe ou contém dados inválidos."
+        )
+        return None
+    if not conteudo:
+        print(
+            "Ainda não existem disciplinas e/ou alunos cadastradas. Primeiro cadastre disciplina e/ou aluno."
+        )
+        return None
+    else:
+        return conteudo
+
+
+def verificar_integridade_arquivos_alunos_disciplinas():
+    students_path = path.students_absolute_path()
+    subjects_path = path.subjects_absolute_path()
+    conteudo_arquivo_alunos = project_file.ler_arquivo(students_path)
+    conteudo_arquivo_disciplinas = project_file.ler_arquivo(subjects_path)
+    if conteudo_arquivo_alunos is None or conteudo_arquivo_disciplinas is None:
+        print(
+            "Não foi possível carregar os dados dos alunos e/ou disciplinas. O arquivo não existe ou contém dados inválidos."
+        )
+        return None, None
+    if not conteudo_arquivo_disciplinas or not conteudo_arquivo_alunos:
+        print(
+            "Ainda não existem disciplinas e/ou alunos cadastradas. Primeiro cadastre disciplina e/ou aluno."
+        )
+        return None, None
+    disciplina_associada_alunos = students.disciplina_associada_a_algum_aluno(
+        conteudo_arquivo_alunos
+    )
+    if not disciplina_associada_alunos:
+        print(
+            "As diciplinas ainda não foram associadas aos alunos. Por favor, primeiro associa as diciplinas que os alunos estão cursando."
+        )
+        return None, None
+    else:
+        return conteudo_arquivo_alunos, conteudo_arquivo_disciplinas
+
+
+def verificar_integridade_para_media_situacao():
+    conteudo_alunos, conteudo_disciplina = (
+        verificar_integridade_arquivos_alunos_disciplinas()
+    )
+    if conteudo_alunos is None or conteudo_disciplina is None:
+        return None
+    else:
+        algum_aluno_com_nota = grades.existe_alguma_nota_cadastrada(conteudo_alunos)
+        if not algum_aluno_com_nota:
+            print(
+                "Ainda não existe nenhuma nota cadastrada a nenhum dos alunos. Por favor cadastre notas primeiro."
+            )
+            return None
+        else:
+            return conteudo_alunos, conteudo_disciplina
